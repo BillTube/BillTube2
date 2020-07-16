175bc87e846f3ef1ff027196b5a5f7ae72471cb9
BillTube <billbt9010@gmail.com>
2020-07-16 02:29:03 +0200

    Update BillTube2.js

--- BillTube2.js
+++ BillTube2.js
@@ -544,17 +544,6 @@ $("#chatwrap").append(
 '<div class="chat-area-footer">' +
 '<div class="chat-area-buttons">' +
 '<div id="chatbox"></div>' +
-//'<svg xmlns="http://www.w3.org/2000/svg" id="settings" viewBox="0 0 24 24" class="chatbtn" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle">' +
-//'<circle cx="12" cy="12" r="3"></circle>' +
-//'<path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z"></path></svg>' +
-//'<rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect></svg>' +
-//'<svg xmlns="http://www.w3.org/2000/svg" id="emotelistbutton" class="chatbtn" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-image">' +
-//'<rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>' +
-//'<circle cx="8.5" cy="8.5" r="1.5"></circle>' +
-//'<path d="M21 15l-5-5L5 21"></path></svg>' +
-//'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" id="smallemotelistbutton" class="chatbtn" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-smile">' +
-//'<circle cx="12" cy="12" r="10"></circle>' +
-//'<path d="M8 14s1.5 2 4 2 4-2 4-2M9 9h.01M15 9h.01"></path></svg>' +
 '</div></div>' );
 
 $("#chatheader").append(
