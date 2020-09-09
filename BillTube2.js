f0714f19305757ab05d7be4dffdf0d093fd8b652
BillTube <billbt9010@gmail.com>
2020-09-09 21:42:37 +0200

    Add files via upload

--- BillTube2.js
+++ BillTube2.js
@@ -4,6 +4,7 @@
 |Special thanks to: Kuer, Xaekai, ss7(supersaw7), kenblu24, Kaba99(Cytube Enhanced) & squabble
 
 */
+
 function showSchedule() {
     $("#schedule").modal();
 }
@@ -234,9 +235,9 @@ ColorsArray = [
 	window.CytubeEnhancedHelpers = function (app) {
 	    var that = this;
 
-var preloadedScript = document.createElement("script");
-preloadedScript.src = "//dl.dropbox.com/s/39iz5fdgzxufp7b/lazyloadbilltheme.js";
-document.body.appendChild(preloadedScript);
+//var preloadedScript = document.createElement("script");
+//preloadedScript.src = "//dl.dropbox.com/s/39iz5fdgzxufp7b/lazyloadbilltheme.js";
+//document.body.appendChild(preloadedScript);
 		/////////////////////
 //Mobile Check
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -612,7 +613,7 @@ $('.form-control').attr('autocomplete' , 'off');
 $("nav .navbar-brand").attr('href', document.URL);
 //$("#mainpage").append("<div id='mHandle-left' class='mHandle'></div> <div id='mHandle-right' class='mHandle'></div><div id='mHandle-mid' class='mHandle'></div>");
 //$("body").prepend("<div id='dragoverlay'><div class='l'></div><div class='r'></div><div id='handleWidget'></div></div>");
-$("#mainpage").append("<style id='splitRatio'>@media (min-width: 992px) {#mainpage > .nano {width: 78%;} #chatwrap {width: 22%;}}</style>");
+$("#mainpage").append("<style id='splitRatio'>@media (min-width: 992px) {#mainpage > .nano {width: 83%;} #chatwrap {width: 17%;}}</style>");
 $("#maincontain").addClass("maincontain");
 $("#sitefooter").append($("#logoutform"));
 $("#logoutform").removeClass("pull-right");
@@ -1137,6 +1138,8 @@ for (i=0;i<field.length;i++){
  window.addEventListener('resize', function() {
     scrollToBottom();
   });
+  
+  
 
 $("#ytapiplayer_html5_api").attr("airplay","allow");
 $("#ytapiplayer_html5_api").attr("x-webkit-airplay","allow");
@@ -21595,159 +21598,6 @@ function emoteToDialog(title, src) {
 	        return;
 	    }
 
-
-	    if ($('#newpollbtn').length !== 0) {
-	        $('#newpollbtn').text(app.t('standardUI[.]Create a poll'));
-	    }
-
-	    if ($('#usercount').length !== 0) {
-	        $('#usercount').text($('#usercount').text().replace('connected users', app.t('standardUI[.]connected users')).replace('connected user', app.t('standardUI[.]connected user')));
-	        window.socket.on('usercount', function () {
-	            $('#usercount').text($('#usercount').text().replace('connected users', app.t('standardUI[.]connected users')).replace('connected user', app.t('standardUI[.]connected user')));
-	        });
-	    }
-	    window.calcUserBreakdown = (function (oldCalcUserBreakdown) {
-	        return function () {
-	            var chatInfo = oldCalcUserBreakdown();
-	            var translatedChatInfo = {};
-
-	            var chatInfoTranslateMap = {
-	                AFK: app.t('standardUI[.]AFK'),
-	                Anonymous: app.t('standardUI[.]Anonymous'),
-	                'Channel Admins': app.t('standardUI[.]Channel Admins'),
-	                Guests: app.t('standardUI[.]Guests'),
-	                Moderators: app.t('standardUI[.]Moderators'),
-	                'Regular Users': app.t('standardUI[.]Regular Users'),
-	                'Site Admins': app.t('standardUI[.]Site Admins')
-	            };
-
-	            for (var chatInfoElement in chatInfo) {
-	                if (chatInfo.hasOwnProperty(chatInfoElement) && typeof chatInfoTranslateMap[chatInfoElement] !== 'undefined') {
-	                    translatedChatInfo[chatInfoTranslateMap[chatInfoElement]] = chatInfo[chatInfoElement];
-	                }
-	            }
-
-	            return translatedChatInfo;
-	        };
-	    })(window.calcUserBreakdown);
-
-	    if ($('#welcome').length !== 0) {
-	        $('#welcome').text(app.t('standardUI[.]Welcome, ') + window.CLIENT.name);
-	    }
-	    if ($('#logout').length !== 0) {
-	        $('#logout').text(app.t('standardUI[.]Log out'));
-	    }
-	    if ($('#username').length !== 0) {
-	        $('#username').attr({placeholder: app.t('standardUI[.]Login')});
-	    }
-	    if ($('#password').length !== 0) {
-	        $('#password').attr({placeholder: app.t('standardUI[.]Password')});
-	    }
-	    if ($('#loginform').find('.checkbox').find('.navbar-text-nofloat').length !== 0) {
-	        $('#loginform').find('.checkbox').find('.navbar-text-nofloat').text(app.t('standardUI[.]Remember me'));
-	    }
-	    if ($('#login').length !== 0) {
-	        $('#login').text(app.t('standardUI[.]Site login'));
-	    }
-
-	    var menuTranslateMap = {
-	        Home: app.t('standardUI[.]Home'),
-	        Account: app.t('standardUI[.]Account'),
-	        Logout: app.t('standardUI[.]Logout'),
-	        Channels: app.t('standardUI[.]Channels'),
-	        Profile: app.t('standardUI[.]Profile'),
-	        'Change Password/Email': app.t('standardUI[.]Change Password/Email'),
-	        Login: app.t('standardUI[.]Log in'),
-	        Register: app.t('standardUI[.]Register'),
-	        Options: app.t('standardUI[.]Options'),
-	        'Channel Settings': app.t('standardUI[.]Channel Settings'),
-	        Layout: app.t('standardUI[.]Layout'),
-	        'Chat Only': app.t('standardUI[.]Chat Only'),
-	        'Remove Video': app.t('standardUI[.]Remove Video')
-	    };
-	    $('.navbar').find('.navbar-nav').children().each(function () {
-	        $(this).find('a').each(function () {
-	            for (var elementToTranslate in menuTranslateMap) {
-	                if (menuTranslateMap.hasOwnProperty(elementToTranslate)) {
-	                    $(this).html($(this).html().replace(elementToTranslate, menuTranslateMap[elementToTranslate]));
-	                }
-	            }
-	        });
-	    });
-
-	    if ($('#mediaurl').length !== 0) {
-	        $('#mediaurl').attr('placeholder', app.t('standardUI[.]Video url'));
-	    }
-	    if ($('#queue_next').length !== 0) {
-	        $('#queue_next').text(app.t('standardUI[.]Next'));
-	    }
-	    if ($('#queue_end').length !== 0) {
-	        $('#queue_end').text(app.t('standardUI[.]At end'));
-	    }
-
-	    $('.qbtn-play').each(function () {
-	        $(this).html($(this).html().replace(/\s*Play/, ' ' + app.t('standardUI[.]Play')));
-	    });
-	    $('.qbtn-next').each(function () {
-	        $(this).html($(this).html().replace(/\s*Queue Next/, ' ' + app.t('standardUI[.]Queue Next')));
-	    });
-	    $('.qbtn-tmp').each(function () {
-	        $(this).html($(this).html().replace(/\s*Make Temporary/, ' ' + app.t('standardUI[.]Make Temporary')).replace(/\s*Make Permanent/, ' ' + app.t('standardUI[.]Make Permanent')));
-	    });
-	    $('.qbtn-delete').each(function () {
-	        $(this).html($(this).html().replace(/\s*Delete/, ' ' + app.t('standardUI[.]Delete')));
-	    });
-	    window.addQueueButtons = (function (oldAddQueueButtons) {
-	        return function (li) {
-	            var result = oldAddQueueButtons(li);
-
-	            if (li.find('.qbtn-play').length !== 0) {
-	                li.find('.qbtn-play').html(li.find('.qbtn-play').html().replace(/\s*Play/, ' ' + app.t('standardUI[.]Play')));
-	            }
-	            if (li.find('.qbtn-next').length !== 0) {
-	                li.find('.qbtn-next').html(li.find('.qbtn-next').html().replace(/\s*Queue Next/, ' ' + app.t('standardUI[.]Queue Next')));
-	            }
-	            if (li.find('.qbtn-tmp').length !== 0) {
-	                li.find('.qbtn-tmp').html(li.find('.qbtn-tmp').html().replace(/\s*Make Temporary/, ' ' + app.t('standardUI[.]Make Temporary')).replace(/\s*Make Permanent/, ' ' + app.t('standardUI[.]Make Permanent')));
-	            }
-	            if (li.find('.qbtn-delete').length !== 0) {
-	                li.find('.qbtn-delete').html(li.find('.qbtn-delete').html().replace(/\s*Delete/, ' ' + app.t('standardUI[.]Delete')));
-	            }
-
-	            return result;
-	        };
-	    })(window.addQueueButtons);
-
-	    this.handleTemp = function (data) {
-	        var tmpBtn = $(".pluid-" + data.uid).find(".qbtn-tmp");
-
-	        if(tmpBtn.length !== 0) {
-	            if(data.temp) {
-	                tmpBtn.html(tmpBtn.html().replace('Сделать временным', app.t('standardUI[.]Make Temporary')));
-	            }
-	            else {
-	                tmpBtn.html(tmpBtn.html().replace('Сделать постоянным', app.t('standardUI[.]Make Permanent')));
-	            }
-	        }
-	    };
-	    window.socket.on('setTemp', function (data) {
-	        that.handleTemp(data);
-	    });
-
-	    if ($('#guestname').length !== 0) {
-	        $('#guestname').attr('placeholder', app.t('standardUI[.]Name'));
-	    }
-	    if ($('#guestlogin')) {
-	        $('#guestlogin').find('.input-group-addon').text(app.t('standardUI[.]Guest login'));
-	    }
-
-	    if ($('#chatbtn').length !== 0) {
-	        $('#chatbtn').text('Отправить');
-	    }
-
-	    if ($('#modflair').length !== 0) {
-	        $('#modflair').text('Цветной ник');
-	    }
 	});
 
 
@@ -21918,7 +21768,7 @@ function emoteToDialog(title, src) {
 	    setTimeout(function() {
             $("#ytapiplayer_html5_api").attr("airplay","allow");
             $("#ytapiplayer_html5_api").attr("x-webkit-airplay","allow");
-            $("#ytapiplayer_html5_api").attr("autoplay","true");
+        //    $("#ytapiplayer_html5_api").attr("autoplay","true");
           var myPlayer = videojs('ytapiplayer_html5_api');
           myPlayer.poster(Poster_URL);
           console.log(myPlayer.poster());
@@ -22367,7 +22217,7 @@ window.cytubeEnhanced.getModule('themes').done(function (extraModules) {
 	        title: 'Halloween Theme',
 	        name: 'Halloween',
 	        cssUrl: '//dl.dropbox.com/s/sc2fswbaam8ufdx/Halloween.css',
-	        jsUrl: '',
+	        jsUrl: '//dl.dropbox.com/s/suz857w35tq4ls4/halloween.js',
 	        pictureUrl: 'https://dl.dropbox.com/s/znqvrttensso4l2/halloween.png'
 	    });
 	});
@@ -22381,7 +22231,7 @@ window.cytubeEnhanced.getModule('themes').done(function (extraModules) {
 	        title: 'Lightweight Theme',
 	        name: 'Lightweight',
 	        cssUrl: '//dl.dropbox.com/s/yj4vgjz3prg67rt/Lightweight.css',
-	        jsUrl: '',
+	        jsUrl: '//dl.dropbox.com/s/p3zgxkncy4hv42t/Lightweight.js',
 	        pictureUrl: 'https://dl.dropbox.com/s/3en6bk2odxtk2tn/lightweight.png'
 	    });
 	});
@@ -22482,13 +22332,13 @@ scrollChat();
 return div;
 }
 
-$( document ).ready(function() {
-function playFile() {
-    var video = $("#ytapiplayer_html5_api");
-    video[0].load();
-    video[0].play();
-};
-});
+//$( document ).ready(function() {
+//function playFile() {
+//    var video = $("#ytapiplayer_html5_api");
+//    video[0].load();
+//    video[0].play();
+//};
+//});
 
 	});
 
