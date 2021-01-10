281e7239e4a0aa4943c834ce389783fed32685ad
BillTube <billbt9010@gmail.com>
2021-01-10 23:43:52 +0100

    Create ChannelConfig.js

---
+++ ChannelConfig.js
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* ----- BILLTUBE THEME CONFIGURATION ----- */
+/* ----- Put this in your channel JS Before the module loader ----- */
+/* ----- custom channel options ----- */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+UI_ChannelName = 0;		
+ChannelName_Caption = 'Channel Name';
+
+UI_ChannelAnnouncement = 0;	
+ChannelAnnouncement_Title = 'custom announcement';
+ChannelAnnouncement_HTML = '<center>This is a custom channel announcement!</center>';
+
+UI_Discord = 0;	
+Discord_NAME = 'name';
+Discord_URL = 'discord url';
+
+UI_Favicon = 0;			
+Favicon_URL = 'favicon url';
+
+/* ----- Channel guide ----- */
+/* ----- requires themoviedb.org account----- */
+var moviedbshow = 0;
+var moviedbkey = 'api key';
+var moviedblist = 'list key';
+
+/* ----- video player ----- */
+var Poster_URL= 'poster url';
+
+/* ----- channel background ----- */
+var BG_Dimmed = 0;
+var BGPics = ['wallpaper url', 'wallpaper url'];
+var BG_Stock= 1;
+
+/* ----- channels slider ----- */
+var UI_ChannelList= 0;
+var Channel_JSON = 'https://cdn.jsdelivr.net/gh/BillTube/BillTube2/channels.json';
